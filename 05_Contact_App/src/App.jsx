e755c7f020d9a3ba69bf514d63c298e4f8d3a6a6
Rajesh Thapa <okthatsall678@gmail.com>
2025-05-01 21:42:48 +0545

    added

--- 05_Contact_App/src/App.jsx
+++ 05_Contact_App/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/navbar";
+import { FaSearch } from "react-icons/fa";
+import { CiCirclePlus } from "react-icons/ci";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className="max-w-[370px] mx-auto px-4">
+      <Navbar />
+      <div className="flex gap-3">
+      <div className="flex relative flex-grow items-center">
+        <FaSearch className="text-white text-3xl absolute ml-1" />
+        <input
+          type="text"
+          className="border bg-transparent border-white rounded h-10 flex-grow text-white pl-9"
+        />
+        <div>
+          <CiCirclePlus className="text-5xl text-white cursor-pointer"/>
+        </div>
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- 05_Contact_App/tailwind.config.js
+++ 05_Contact_App/tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors:{
+        gray:"#5A5959",
+        yellow:"#FFEAAE",
+        "dark-yellow":"#FCCA3F",
+        orange: "#F6820C",
 
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+}
