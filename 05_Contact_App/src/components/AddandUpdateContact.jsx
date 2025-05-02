623db2c02eae591899f3f949b61ff0d0a3a4a8ad
Rajesh Thapa <okthatsall678@gmail.com>
2025-05-02 10:15:39 +0545

    AddContact functionality added

--- 05_Contact_App/src/components/AddandUpdateContact.jsx
+++ 05_Contact_App/src/components/AddandUpdateContact.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
 import Modal from './Modal'
+import { Field,Form,Formik } from 'formik'
+import { collection } from 'firebase/firestore'
+import { db } from '../config/firebase'
+import { addDoc } from 'firebase/firestore'
+ 
 
 
 const AddandUpdateContact = ({isOpen, onClose}) => {
+
+    const addContact = async (contact)=>{
+        try{
+            const contactRef= collection(db,"contacts")
+            await addDoc(contactRef, contact)
+        }
+        catch(error){
+            console.log(error);
+            
+        }
+    }
   return (
     <div>
       <Modal isOpen={isOpen}
     onClose={onClose}>
-      HI
+    <Formik 
+    initialValues={{
+        name:"",
+        email:""
+    }}
+    onSubmit={(values)=>{
+        console.log(values);
+        addContact(values)
+        
+    }}>
+        <Form className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-1'>
+            <label htmlFor="name">Name</label>
+            <Field name="name" className="border h-10"/>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+            <label htmlFor="email">Email</label>
+            <Field name="email" className="border h-10"/>
+            </div>
+            <button className='bg-orange px-3 py-1.5 border self-end'>
+                Add contact
+            </button>
+        </Form>
+    </Formik>
     </Modal>
     </div>
   )
