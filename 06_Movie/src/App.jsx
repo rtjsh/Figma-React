d14bac3a010dc0826ca1e48411c31a47c69dfb98
Rajesh Thapa <okthatsall678@gmail.com>
2025-05-13 11:03:24 +0545

    header setup successfully

--- 06_Movie/src/App.jsx
+++ 06_Movie/src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [SearchTerm, setSearchTerm] = useState("");
   return (
     <main>
         <div className='pattern'>
@@ -9,7 +11,7 @@ const App = () => {
                     <img src="/images/hero.png" alt="" />
                     <h1>Find <span className='text-gradient'>Movie</span> You'll enjoy Without the Hassle</h1>
                 </header>
-                <p>Search</p>
+                <Search SearchTerm={SearchTerm} setSearchTerm={setSearchTerm}/>
             </div>
         </div>
     </main>
